7ddfe65686389542bafbbd26d65321725ac2f1ee
ACJ <gitlab@acjs.net>
2021-03-12 11:13:30 +0100

    Use Gulp Twig rather than Twig through composer

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var pipeline = require('readable-stream').pipeline;
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var twig = require('gulp-twig');
 
 sass.compiler = require('node-sass');
 
@@ -17,6 +18,12 @@ gulp.task('sass', function () {
         .pipe(sass({includePaths: ['node_modules'], outputStyle: 'compressed'}))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('twig', function () {
+    return gulp.src(['./templates/*.twig', 'templates'])
+        .pipe(twig({extname: false}))
+        .pipe(gulp.dest('demo'));
 });
 
 gulp.task('sass:watch', function () {
